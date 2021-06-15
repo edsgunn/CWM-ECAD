6b47c8d041f9899702bd725f01bea5266f7ed2bd
Cloud User <centos@ip-172-31-13-190.eu-west-2.compute.internal>
2021-06-15 09:32:13 +0000

    Completed excercise 4

--- Ex4/top.v
+++ Ex4/top.v
@@ -16,3 +16,31 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module controller (
+    //Todo: add ports
+    input rst,clk,button,
+    output colour
+    );
+                    
+    //Todo: add registers and wires, if needed
+    reg [2:0] colour;
+
+    //Todo: add user logic
+    always @(posedge clk) begin
+        if (colour == 3'b000 || colour == 3'b000 || rst)
+            colour = 3'b001;
+    end
+    always @(posedge button) begin
+      case(colour)
+          3'b001: colour = 3'b010;
+          3'b010: colour = 3'b011;
+          3'b011: colour = 3'b100;
+          3'b100: colour = 3'b101;
+          3'b101: colour = 3'b110;
+          3'b110: colour = 3'b001;
+          default: colour = 3'b001;
+      endcase
+    end     
+endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -7,3 +7,72 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+    
+//Todo: Parameters
+parameter CLK_PERIOD = 10;
+//Todo: Regitsers and wires
+reg rst;
+reg button;
+reg clk;
+reg err;
+wire [3:0] colour;
+reg [3:0] prev_colour;
+//Todo: Clock generation
+initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+//Todo: User logic
+initial begin
+    rst=1;
+    button=0;
+    err=0;
+    #(CLK_PERIOD)
+    if (colour != 3'b001) begin
+        $display("***TEST FAILED! rst==%d, colour=%d ***",rst,colour);
+        err=1;
+    end
+    prev_colour = colour;
+    rst = 0;
+    #(3*CLK_PERIOD)
+    if (colour != prev_colour) begin
+        $display("***TEST FAILED! button==%d, colour=%d prev_colour=%d ***",button,colour,prev_colour);
+        err=1; 
+    end
+    forever begin
+        prev_colour = colour;
+        button = 1;
+        #(CLK_PERIOD)
+        button = 0;
+        #(CLK_PERIOD)
+        if (colour != (prev_colour + 1) && prev_colour < 3'b110) begin
+            $display("***TEST FAILED! colour=%d prev_colour=%d ***",colour,prev_colour);
+            err = 1;
+        end
+        if (prev_colour == 3'b110 && colour != 3'b001) begin
+            $display("***TEST FAILED! colour=%d prev_colour=%d ***",colour,prev_colour);
+            err = 1;
+        end
+    end
+end
+//Todo: Finish test, check for success
+initial begin
+        #(60*CLK_PERIOD)
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
+//Todo: Instantiate counter module
+controller top (
+   .rst (rst),
+   .button (button),
+   .clk (clk),
+   .colour (colour)
+);
+endmodule 
