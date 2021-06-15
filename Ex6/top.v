6f5ad48bdd017c691020faf6ecd7359694804493
Cloud User <centos@ip-172-31-13-190.eu-west-2.compute.internal>
2021-06-15 15:53:42 +0000

    progress on excercise 6

--- Ex6/top.v
+++ Ex6/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #6 - RGB Colour Converter
-// Student Name:
-// Date: 
+// Student Name: Edward Gunn
+// Date: 15/06/21
 //
 //
 //  Description: In this exercise, you need to design a memory with 8 entries, 
@@ -15,3 +15,20 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module rgb_converter (
+    //Todo: add ports
+    input clk, colour, enable,
+    output rgb 
+    );
+   
+blk_mem_gen_0 your_instance_name (
+  .clka(clk),    // input wire clka
+  .ena(enable),      // input wire ena
+  .wea(1'b0),      // input wire [0 : 0] wea
+  .addra(colour),  // input wire [2 : 0] addra
+  .dina(24'd0),    // input wire [23 : 0] dina
+  .douta(rgb)  // output wire [23 : 0] douta
+);
+endmodule
--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -7,3 +7,60 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+	);
+
+parameter CLK_PERIOD = 10;
+
+reg clk;
+reg enable;
+reg [2:0] colour;
+reg err;
+wire [23:0] rgb;
+reg [23:0] rgb_prev;
+  
+initial begin
+  clk = 1'b0;
+  forever
+      #(CLK_PERIOD/2) clk=~clk;
+end
+
+initial begin
+    err = 0;
+    enable = 0;
+    colour = 3'b001;
+    #(CLK_PERIOD)
+
+    if (rgb != 24'b0) begin
+	$display("***TEST FAILED :(***");
+	err = 1;
+    end
+
+    forever begin
+        enable = 1;
+        rgb_prev = rgb;
+        colour = colour + 3'd1;
+        #(CLK_PERIOD)
+        if (rgb_prev == rgb) begin
+            $display("***TEST FAILED :(***");
+	    err = 1;
+        end
+    end
+end
+  
+ initial begin
+    #500
+    if (err==0)
+      $display("***TEST PASSED! :) ***");
+      $finish;
+ end
+     
+rgb_converter top (.enable (enable),
+	       .colour (colour),
+	       .rgb (rgb),
+	       .clk (clk)
+	       );
+
+endmodule 
